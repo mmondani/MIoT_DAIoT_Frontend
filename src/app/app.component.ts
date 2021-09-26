cbf0865103f9dc2edb517364c854415fb525a4b4
mmondani <mmondani89@gmail.com>
2021-09-26 11:33:54 -0300

    se agrega la navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'daiot';
+  public isCollapsed: boolean = false;
+
+  public toggleCollapse () {
+    this.isCollapsed = !this.isCollapsed;
+    console.log(this.isCollapsed);
+  }
 }
